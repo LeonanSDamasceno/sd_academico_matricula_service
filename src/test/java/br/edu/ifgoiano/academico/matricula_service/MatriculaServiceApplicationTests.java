6d0f7226cd0244a1e4021ff45822e22af3e66779
Artur Duarte Monteiro <monteiroarturduarte@gmail.com>
2026-05-28 08:48:51 -0300

    Initial commit

---
+++ src/test/java/br/edu/ifgoiano/academico/matricula_service/MatriculaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.matricula_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
